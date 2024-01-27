ada41fbeecffc4130b0249daabc5393a06164d77
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2024-01-28 00:05:55 +0600

    refractor game grid

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -19,11 +19,10 @@ const GameGrid = ({gameQuery}:Props) => {
     const skeletons=[1,2,3,4,5,6]
 
 
+    if(error) return <Text>{error}</Text>
 
   return (
-    <>
-        {error && <Text>{error}</Text>}
-        <SimpleGrid padding="10px" columns={{
+    <SimpleGrid padding="10px" columns={{
           sm:1,
           md:2,
           lg:3,
@@ -37,7 +36,6 @@ const GameGrid = ({gameQuery}:Props) => {
             <GameCard game={game} />
           </GameCardContainer>))}
         </SimpleGrid>
-    </>
   )
 }
 
