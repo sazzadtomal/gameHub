3a325d96a8a49a4e697732316593bde2e456d02b
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2024-02-05 19:22:49 +0600

    modified query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,7 +24,7 @@ export interface Game{
 
 
 const useGames = (gameQuery:GameQuery) =>useData<Game>("/games",{params:{genres:gameQuery?.genre?.id, 
-    platforms:gameQuery?.platform?.id,
+    parent_platforms:gameQuery?.platform?.id,
     ordering:gameQuery.sortOrder,
     search:gameQuery.searchText
 }},[gameQuery])
